33c50f579d4459c32a36fceb08541cdd986b1297
Shourya Bansal <shouryabansal2012@gmail.com>
2020-06-25 14:48:32 -0400

    Restruction IO Files

---
+++ Java/src/usaco/practice/previousContests/years1819/december/Convention.java
@@ -0,0 +1,19 @@
+package previousContests.years1819.december;
+
+/*
+ * Farmer John is hosting a new bovine grass-eating convention at his farm!
+ *
+ * Cows from all over the world are arriving at the local airport to attend the convention and eat grass. Specifically,
+ * there are N cows arriving at the airport (1≤N≤105) and cow i arrives at time ti (0≤ti≤109). Farmer John has arranged M (1≤M≤105)
+ * buses to transport the cows from the airport. Each bus can hold up to C cows in it (1≤C≤N). Farmer John is waiting with the buses
+ * at the airport and would like to assign the arriving cows to the buses. A bus can leave at the time when the last cow on it arrives.
+ * Farmer John wants to be a good host and so does not want to keep the arriving cows waiting at the airport too long. What is the smallest
+ * possible value of the maximum waiting time of any one arriving cow if Farmer John coordinates his buses optimally? A cow’s waiting time
+ * is the difference between her arrival time and the departure of her assigned bus.
+ *
+ * It is guaranteed that MC≥N.
+ */
+
+public class Convention {
+    
+}
