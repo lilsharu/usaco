5f9df95b2528dc56d221936a4672b9d8a1e6fff0
Shourya Bansal <shouryabansal2012@gmail.com>
2020-08-10 15:03:01 -0400

    Finished the Bovine Shuffle

--- CPP/Book/5/problems/theBovineShuffle.cpp
+++ CPP/Book/5/problems/theBovineShuffle.cpp
@@ -4,16 +4,7 @@ using namespace std;
 
 const int MAX_N = 100;
 
-int n, pos[MAX_N];
-
-struct cow {
-    int pos;
-    int id;
-
-    bool operator < (const cow& a) const {
-        return this->pos < a.pos;
-    }
-};
+int n, pos[MAX_N], cows[MAX_N], newCows[MAX_N];
 
 int main() {
     ios::sync_with_stdio(false);
@@ -24,6 +15,30 @@ int main() {
     cin >> n;
     
     for (int i = 0; i < n; i++) {
-        cin >> pos[i];
+        int indexTo;
+        cin >> indexTo;
+
+        pos[indexTo - 1] = i;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        int cow;
+        cin >> cow;
+
+        cows[i] = cow;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < n; i++) {
+            newCows[pos[i]] = cows[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cows[i] = newCows[i];
+        }
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        cout << cows[i] << '\n';
     }
 }
