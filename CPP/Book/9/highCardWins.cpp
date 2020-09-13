5f47b747bd2a849423dfd3a31f2bb5921c481f34
Shourya Bansal <shouryabansal2012@gmail.com>
2020-09-12 22:37:28 -0400

    Finished High Card with 1/15

--- CPP/Book/9/highCardWins.cpp
+++ CPP/Book/9/highCardWins.cpp
@@ -6,6 +6,9 @@
 using namespace std;
 
 int indexUp(vi arr, int toBeat) {
+    if (arr.size() == 1 || arr.size() == 0) return 0;
+    if (arr.size() == 2) return 1;
+    if (arr.size() == 3) return 2;
     int n = 0, k = arr.size() - 1, key = n/k;
     while (n < k) {
         key = n/k;
@@ -28,23 +31,42 @@ int main() {
     int n;
     cin >> n;
 
-    vi elsie(n), bessie(n);
+    vi elsie(n), bessie(n), calculator(n);
 
+    int temp; 
     for (int i = 0; i < n; i++) {
-        int temp; 
         cin >> temp;
-
         elsie[i] = temp;
+        calculator[i] = temp;
     }
+    sort(calculator.begin(), calculator.end());
 
-    int eIndex = 0, bIndex;
-    for (int i = 0; i < 2 * n; i++) {
-        if (elsie[eIndex] == i) {
+    int eIndex = 0, bIndex = 0;
+    for (int i = 1; i <= 2 * n; i++) {
+        if (calculator[eIndex] == i) {
             eIndex++;
         } else {
             bessie[bIndex++] = i;
         }
     }
 
+    int points = 0;
+    for (int i = 0; i < n; i++) {
+        int elsieCard = elsie[i];
+        int index = indexUp(bessie, elsieCard);
+        if (!(index - 1 >= bessie.size()) && bessie[index - 1] > elsieCard) {
+            bessie.erase(bessie.begin() + index - 1);
+            points++;
+        } else if (!(index >= bessie.size()) && bessie[index] > elsieCard) {
+            bessie.erase(bessie.begin() + index);
+            points++;
+        } else if (!(index + 1 >= bessie.size()) && bessie[index + 1] > elsieCard) {
+            bessie.erase(bessie.begin() + index + 1);
+            points++;
+        } else {
+            bessie.erase(bessie.begin());
+        }
+    }
 
+    cout << points << '\n';
 }
