792b74cc7efebba00e0fe514757206fd0dbb652c
Shourya Bansal <shouryabansal2012@gmail.com>
2020-09-12 21:49:48 -0400

    Set up file for High Card Wins

---
+++ CPP/Book/9/highCardWins.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    freopen("highcard.in", "r", stdin);
+    freopen("highcard.out", "w", stdout);
+
+
+}
