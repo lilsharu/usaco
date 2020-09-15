f2ec1bc50b0120fc0e8407f0d820e264e21255d2
Shourya Bansal <shouryabansal2012@gmail.com>
2020-09-15 12:50:12 -0400

    Fixed Errors and got 10/10
    It turns out that long long is necessary. Who knew?

--- CPP/Book/9/restStops.cpp
+++ CPP/Book/9/restStops.cpp
@@ -4,29 +4,49 @@
 
 using namespace std;
 
+long long n, l, rf, rb;
+int pos[1000000], c[1000000];
+bool isMax[100000];
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     freopen("reststops.in", "r", stdin);
     freopen("reststops.out", "w", stdout);
 
-    int l, n, rf, rb;
     cin >> l >> n >> rf >> rb;
 
-    int maxTastiness = 0, pf = 0, pb = 0;
-    
-    for (int i = 0; i < n; i++) {
-        int pos, taste;
-        cin >> pos >> taste;
+    for (int i = 0; i < n; i++) cin >> pos[i] >> c[i];
 
-        int secs = (pos - pb) * rb;
-        int distanceForFarmer = pos - pf;
-        int secsToGo = distanceForFarmer * rf - secs;
-
-        maxTastiness += (int) secsToGo * taste;
-
-        pf += distanceForFarmer, pb = pos;
+    int max = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        if (c[i] > max) {
+            isMax[i] = 1;
+            max = c[i];
+        }
     }
+
+    long long maxTastiness = 0, lastx = 0, tf = 0, tb = 0;
+
+    for(int i = 0; i < n; i++) if(isMax[i]) {
+		tb += (pos[i] - lastx) * rb;
+		tf += (pos[i] - lastx) * rf;
+		maxTastiness += (tf - tb) * c[i];
+		tb = tf;
+		lastx = pos[i];
+	}
+
+    // for (int i = 0; i < n; i++) {
+    //     if (isMax[i]) {
+    //         int position = pos[i];
+    //         int movement = pos[i] - lastx;
+
+    //         int secsToGo = movement * rf - movement * rb;
+
+    //         maxTastiness += secsToGo * c[i];
+    //         lastx = pos[i];
+    //     }
+    // }
 
     cout << maxTastiness << '\n';
 }
