4f3dcfabf710ac2a543642085a8283ebaf0ab45b
Shourya Bansal <shouryabansal2012@gmail.com>
2020-09-15 14:00:08 -0400

    Fixed Issues in Why did the Cow Cross the Road
    Looked at solution

--- CPP/Book/9/whyDidTheCowCrossTheRoad.cpp
+++ CPP/Book/9/whyDidTheCowCrossTheRoad.cpp
@@ -12,23 +12,9 @@ bool cowComp(specialCow a, specialCow b) {
 }
 
 
-ll n, c;
-vector<ll> chickens;
-specialCow cows[20000];
-
-int search(ll min, ll max, ll low,ll high) {
-    int mid = (high + low)/2;
-    if (chickens[mid] >= min && chickens[mid] <= max) {
-        return mid;
-    }
-
-    if (high < low) return -1;
-
-    if (chickens[mid] > max) {
-        return search(min, max, low, mid - 1);
-    }
-    else return search(min, max, mid + 1, high);
-}
+int n, c;
+multiset<int> chickens;
+vector<pair<int, int>> cows;
 
 int main() {
     ios::sync_with_stdio(false);
@@ -38,29 +24,27 @@ int main() {
 
     cin >> c >> n;
 
+    int x, y, total = 0;
+
     for (int i = 0; i < c; i++) {
-        ll temp;
-        cin >> temp;
-        chickens.push_back(temp);
+        cin >> x;
+        chickens.insert(x);
     }
 
     for (int i = 0; i < n; i++) {
-        specialCow c;
-        cin >> c.min >> c.max;
-        cows[i] = c;
+        cin >> x >> y;
+        cows.push_back(pair<int, int>(y, x));
     }
 
-    sort(cows, cows + n, cowComp);
-    sort(chickens.begin(), chickens.end());
+    sort(cows.begin(), cows.end());
 
-    int nums = 0;
     for (int i = 0; i < n; i++) {
-        int chickIndex = search(cows[i].min, cows[i].max, 0, c - 1);
-        if (chickIndex == -1) continue;
-        nums++;
-        if (i + 1 <= chickens.size())
-            chickens.erase(chickens.begin() + i);
+        auto which_chicken = chickens.lower_bound(cows[i].second);
+        if (which_chicken != chickens.end() && *which_chicken <= cows[i].first) {
+            total++;
+            chickens.erase(which_chicken);
+        }
     }
 
-    std::cout << nums << '\n';
+    cout << total;
 }
