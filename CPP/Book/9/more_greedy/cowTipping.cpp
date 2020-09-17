c92be555ec7bfaa0067c8e6985c035e772d73578
Shourya Bansal <shouryabansal2012@gmail.com>
2020-09-17 10:31:07 -0400

    Started Working on More Greedy Problems for Practice
    I am starting Cow Tipping

---
+++ CPP/Book/9/more_greedy/cowTipping.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+bool cows[10][10];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    freopen("cowtip.in", "r", stdin);
+    freopen("cowtip.out", "w", stdout);
+
+
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < n; k++) {
+            cin >> cows[i][k];
+        }
+    }
+
+    
+}
