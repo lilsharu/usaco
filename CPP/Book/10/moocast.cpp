3ff41911433746f400342690a60bbda66d35674c
Shourya Bansal <shouryabansal2012@gmail.com>
2020-10-10 18:21:13 -0400

    Creates File for Task Moocast

---
+++ CPP/Book/10/moocast.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    freopen("moocast.in", "r", stdin);
+    freopen("moocast.out", "w", stdout);
+
+}
