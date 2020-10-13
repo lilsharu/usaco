a68ede6f17696e8ec28461922329cdfa45116bf1
Shourya Bansal <shouryabansal2012@gmail.com>
2020-10-12 21:51:12 -0400

    Starts debugging Closing the Farm
    Implements some pointer voodoo to create interreferencing.

--- CPP/Book/10/closingTheFarm.cpp
+++ CPP/Book/10/closingTheFarm.cpp
@@ -2,35 +2,42 @@
 
 using namespace std;
 
-struct barn {
+struct barn
+{
     int pos;
     bool isOpen;
-    barn(int _pos): pos(_pos) {}
+    barn(int _pos) : pos(_pos), isOpen(true) {}
     barn() {}
 
-    bool operator<(const barn& a) const {
+    bool operator<(const barn &a) const
+    {
         return pos < a.pos;
-    } 
+    }
 };
 
 int n, m;
 int current = 0;
 
-barn barns[3000];
-vector<barn> adj[3000];
+barn barns[4];
+vector<barn *> adj[4];
 set<barn> opened;
-bool visited[3000][3000];
+bool visited[4][4];
 
-void dfs(int node) {
+void dfs(int node)
+{
     visited[current][node] = true;
-    for (barn next : adj[node]) {
-        if (!visited[next.pos] && next.isOpen) {
-            dfs(next.pos);
+    for (barn *next : adj[node])
+    {
+        barn n = *next;
+        if (!visited[current][n.pos] && n.isOpen)
+        {
+            dfs(n.pos);
         }
     }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     freopen("closing.in", "r", stdin);
@@ -38,31 +45,40 @@ int main() {
 
     cin >> n >> m;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         barns[i] = barn(i);
         opened.insert(barns[i]);
     }
-    
+
     int a, b;
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++)
+    {
         cin >> a >> b;
-        adj[a].push_back(barns[b]);
-        adj[b].push_back(barns[a]);
+        a--, b--;
+        adj[a].push_back(&barns[b]);
+        adj[b].push_back(&barns[a]);
     }
-    delete &a;
-    delete &b;
 
     int toClose;
-    loop:
-    for (int i = 0; i < n; i++) {
-        cin >> toClose;
-        toClose--;
-        opened.erase(barns[toClose]);
+loop:
+    for (int i = 0; i < n; i++)
+    {
+        if (i != 0)
+        {
+            cin >> toClose;
+            toClose--;
+            opened.erase(barns[toClose]);
+            barns[toClose].isOpen = false;
+        }
         
-        current = (*opened.begin()).pos;
-        dfs(current);
-        for (int j = 0; j < n; j++) {
-            if (barns[j].isOpen && !visited[i][barns[j].pos]) {
+        current = i;
+        int toSearchFrom = (*opened.begin()).pos;
+        dfs(toSearchFrom);
+        for (int j = 0; j < n; j++)
+        {
+            if (barns[j].isOpen && !visited[i][barns[j].pos])
+            {
                 cout << "NO" << '\n';
                 goto loop;
             }
