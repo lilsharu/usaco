3d9556aa5272931a9aa91e538b9494df8759b603
Shourya Bansal <shouryabansal2012@gmail.com>
2020-10-29 19:16:53 -0400

    Debuggs issues in MilkVisists

--- CPP/Book/10/milkVisitsAfterSol.cpp
+++ CPP/Book/10/milkVisitsAfterSol.cpp
@@ -5,12 +5,9 @@ typedef vector<int> vi;
 
 #define FOR(i,a,b) for (int i = (a); i < (b); ++i)
 #define F0R(i,a) FOR(i,0,a)
-#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
-#define R0F(i,a) ROF(i,0,a)
 #define trav(a,x) for (auto& a: x)
 
 #define pb push_back
-#define all(x) begin(x), end(x)
 
 void setIO(string name) {
 	ios_base::sync_with_stdio(0); cin.tie(0);
@@ -43,7 +40,7 @@ int main() {
     string text; cin >> text;
 
     FOR(i, 1, n + 1) {
-        chars[i] = text[i];
+        chars[i] = text[i - 1];
     }
     F0R(i, n - 1) {
         int a, b;
@@ -57,13 +54,13 @@ int main() {
             dfs(i); 
         }
     }
-    FOR(i, 1, n + 1) {
+    F0R(i, m) {
         int a, b;
         char c;
         cin >> a >> b >> c;
 
-        if (chars[a] == c || comp[a] != comp[b]) cout << '1';
-        else cout << '0';        
+        if (chars[a] == c || comp[a] != comp[b]) cout << 1;
+        else cout << 0;        
     }
     cout << '\n';
     return 0;
