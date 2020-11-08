e4d4c02372f81d81e14bca1de25366571fab210c
Shourya Bansal <shouryabansal2012@gmail.com>
2020-11-08 12:10:23 -0500

    Solve CF 279B Books Problem

---
+++ CPP/USACO.guide/Two Pointers/booksCF279B.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef long long ll;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define trav(a, x) for (auto &a : x)
+
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+#define endl '\n';
+
+void setIO() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+int main() {
+    setIO();
+    int n, m;
+    cin >> n >> m;
+
+    vi arr(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int l = 0, r = 0, sum = 0, sol = 0;;
+    for (r = 0; r < n; r++) {
+        sum += arr[r];
+        while (sum > m && l < r) {
+            sum -= arr[l++];
+        }
+        sol = max(sol, r - l + 1);
+    }
+
+    cout << sol << endl;
+}
