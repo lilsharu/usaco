5382a61a804ecccdb7899fb1343cde00053110be
Shourya Bansal <shouryabansal2012@gmail.com>
2020-11-17 18:59:00 -0500

    Fix Diamond Collector with Assistance

--- CPP/USACO.guide/Two Pointers/diamondCollector.cpp
+++ CPP/USACO.guide/Two Pointers/diamondCollector.cpp
@@ -30,41 +30,30 @@ int main() {
     int n, k;
     cin >> n >> k;
 
-    vi arr(n + 1);
+    vi arr(n);
     
     F0R(i, n) cin >> arr[i];
-    arr[n] = INT_MAX - arr[n - 1];
 
     sort(all(arr));
     
-    int l = 0, r = 0;
-
     vi maxVals(n, 1);
+
+    int l = 0, r = 0;
+    for (; l < n; l++) {
+        while (r < n && arr[r] - arr[l] <= k) r++;
+        maxVals[l] = r - l;
+    }
     
+    vi maxValAfter(n + 1);
+    maxValAfter[n] = 0;
+    R0F(i, n) {
+        maxValAfter[i] = max(maxValAfter[i + 1], maxVals[i]);
+    }
+    
+    int ans = 0;
     F0R(i, n) {
-        int j = i + 1;
-        while (arr[j++] - arr[i] <= k) {
-            maxVals[i]++;
-        }
+        ans = max(ans, maxVals[i] + maxValAfter[i + maxVals[i]]);
     }
 
-    int max = 0, m = 0;
-    F0R(i, n) {
-        if (maxVals[i] > max) {
-            max = maxVals[i];
-            m = i;
-        }
-    }
-
-    int max2 = 0;
-    F0R(i, n) {
-        if (i >= m && i < m + max - 1) {
-            continue;
-        }
-        if (maxVals[i] > max2) {
-            max2 = maxVals[i];
-        }
-    }
-
-    cout << max + max2 << endl;
+    cout << ans << endl;    
 }
