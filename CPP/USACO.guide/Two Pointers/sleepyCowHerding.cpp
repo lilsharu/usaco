710c3dc6179dbb2750182dbaba7f7164656eb958
Shourya Bansal <shouryabansal2012@gmail.com>
2020-11-18 16:51:57 -0500

    Create: Sleepy Cow Herding USACO Feb2019

---
+++ CPP/USACO.guide/Two Pointers/sleepyCowHerding.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define F0R(i, a) FOR(i, 0, a)
+#define FORN FOR(i, 0, n)
+#define ROF(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i, a) ROF(i, 0, a)
+#define ROFN ROF(i, 0, n)
+#define trav(a, x) for (auto &a : x)
+
+#define pb push_back
+#define all(x) begin(x), end(x)
+
+#define read(a) int a; cin >> a
+#define read2(a, b) read(a); read(b)
+#define read3(a, b, c) read2(a, b); read(c)
+#define read4(a, b, c, d) read3(a, b, c), read(d)
+
+#define endl '\n';
+
+void setIO(string name) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen((name + ".in").c_str(), "r", stdin);
+    freopen((name + ".out").c_str(), "w", stdout);
+}
+
+int main() {
+    setIO("herding");
+
+    read(n);
+    
+    vi arr(n);
+
+    FORN {
+        cin >> arr[i];
+    }
+
+    sort(all(arr));
+    
+    int subtractBy = arr[0];
+    int maximum = arr[n - 1] - subtractBy + 1;
+    
+    vi positions(maximum);
+
+    FORN {
+        positions[arr[i] - subtractBy] = 1;
+    }
+
+    vi sum(maximum + 1);
+    
+    FOR(i, 1, maximum + 1) {
+        sum[i] = positions[i - 1] + sum[i - 1];
+    }
+
+    int s = 0;
+    FOR(i, n + 1, maximum + 1) {
+        int val = sum[i] - sum[i - n - 1];        
+        if (val > s) {
+            s = val;
+        }
+    }
+
+    int minimum = n - s;
+    int maxGap = max(arr[n - 2] - arr[0], arr[n - 1], arr[1]) - n + 2;
+
+    cout << minimum << endl;
+    cout << maxGap << endl;
+}
