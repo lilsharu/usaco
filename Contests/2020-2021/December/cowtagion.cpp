def25c2a42eda722a89e935ed1b0aa6da0c43186
Shourya Bansal <shouryabansal2012@gmail.com>
2020-12-18 17:39:54 -0500

    Revert to Original Working Version

--- Contests/2020-2021/December/cowtagion.cpp
+++ Contests/2020-2021/December/cowtagion.cpp
@@ -1,3 +1,61 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// typedef vector<int> vi;
+// typedef long long ll;
+
+// #define pb push_back
+// #define all(x) begin(x), end(x)
+
+// #define endl '\n';
+
+// int n, ps = 0;
+// vi adj[100000];
+// int dist[100000];
+
+// void bfs(int start) {
+//     memset(dist, -1, sizeof dist);
+//     queue<int> q;
+//     dist[start] = 0;
+//     q.push(start);
+    
+//     while (!q.empty()) {
+//         int v = q.front();
+//         q.pop();
+//         int sum = 0;
+//         for (int e : adj[v]) {
+//             if (dist[e] == -1) {
+//                 dist[e] = dist[v] + 1;
+//                 // ps += dist[e];
+//                 q.push(e);
+//                 sum++;
+//             }
+//         }
+//         ps += (sum + (sum == 0 ? 0 : ceil(log2(sum))));
+//     }
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(0);
+
+//     cin >> n;
+
+//     for (int i = 0; i < n - 1; i++) {
+//         int a, b;
+//         cin >> a >> b;
+//         a--; b--;
+//         adj[a].pb(b);
+//         adj[b].pb(a);
+//     }
+
+//     // int getCows = ceil(log2(n));
+    
+//     bfs(0);
+    
+//     cout << ps << endl;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -22,16 +80,13 @@ void bfs(int start) {
     while (!q.empty()) {
         int v = q.front();
         q.pop();
-        int sum = 0;
         for (int e : adj[v]) {
             if (dist[e] == -1) {
                 dist[e] = dist[v] + 1;
-                // ps += dist[e];
+                ps += dist[e];
                 q.push(e);
-                sum++;
             }
         }
-        ps += (sum + (sum == 0 ? 0 : ceil(log2(sum))));
     }
 }
 
@@ -49,9 +104,9 @@ int main() {
         adj[b].pb(a);
     }
 
-    // int getCows = ceil(log2(n));
+    int getCows = ceil(log2(n));
     
     bfs(0);
     
-    cout << ps << endl;
+    cout << getCows + ps << endl;
 }
