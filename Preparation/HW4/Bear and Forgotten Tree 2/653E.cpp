c6316ff9cd9fbd1c41ced4371f2eb756de4ee788
Shourya Bansal <shouryabansal2012@gmail.com>
2021-03-22 21:10:10 -0400

    Start 653E

---
+++ Preparation/HW4/Bear and Forgotten Tree 2/653E.cpp
@@ -0,0 +1,311 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using db = double;
+using str = string; // yay python!
+
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<ld>;
+using vs = vector<str>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vpd = vector<pd>;
+
+#define tcT template<class T
+#define tcTU tcT, class U
+
+tcT> using V = vector<T>;
+tcT, size_t SZ> using AR = array<T,SZ>;
+tcT> using PR = pair<T,T>;
+
+// pairs
+#define mp make_pair
+#define f first
+#define s second
+
+// easy endl
+#define endl '\n'
+
+// vectors
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pb push_back
+#define eb emplace_back
+#define pf push_front
+
+#define lb lower_bound
+#define ub upper_bound
+tcT> int lwb(V<T>& a, const T& b) { return int(lb(all(a),b)-bg(a)); }
+
+// loops
+#define ffor(i,a,b) for (int i = (a); i < (b); ++i)
+#define ff0r(i,a) ffor(i,0,a)
+#define rrof(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define rr0f(i,a) rrof(i,0,a)
+#define fforn ff0r(i, n)
+#define trav(a,x) for (auto& a: x)
+
+const int MOD = 1e9+7; // 998244353;
+const int MX = 2e5+5;
+const ll INF = 1e18; // not too close to LLONG_MAX
+const db PI = acos((db)-1);
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1}; // for every grid problem!!
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>;
+
+// bitwise ops
+// also see https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { // assert(x >= 0); // make C++11 compatible until USACO updates ...
+	return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x))
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+ll cdiv(ll a, ll b) { return a/b+((a^b)>0&&a%b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a/b-((a^b)<0&&a%b); } // divide a by b rounded down
+
+tcT> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+tcT> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; }
+
+tcTU> T fstTrue(T lo, T hi, U f) {
+	hi ++; assert(lo <= hi); // assuming f is increasing
+	while (lo < hi) { // find first index such that f is true
+		T mid = lo+(hi-lo)/2;
+		f(mid) ? hi = mid : lo = mid+1;
+	}
+	return lo;
+}
+tcTU> T lstTrue(T lo, T hi, U f) {
+	lo --; assert(lo <= hi); // assuming f is decreasing
+	while (lo < hi) { // find first index such that f is true
+		T mid = lo+(hi-lo+1)/2;
+		f(mid) ? lo = mid : hi = mid-1;
+	}
+	return lo;
+}
+tcT> void remDup(vector<T>& v) { // sort and remove duplicates
+	sort(all(v)); v.erase(unique(all(v)),end(v)); }
+tcTU> void erase(T& t, const U& u) { // don't erase
+	auto it = t.find(u); assert(it != end(t));
+	t.erase(it); } // element that doesn't exist from (multi)set
+
+// INPUT
+#define tcTUU tcT, class ...U
+tcT> void re(complex<T>& c);
+tcTU> void re(pair<T,U>& p);
+tcT> void re(V<T>& v);
+tcT, size_t SZ> void re(AR<T,SZ>& a);
+
+tcT> void re(T& x) { cin >> x; }
+void re(double& d) { str t; re(t); d = stod(t); }
+void re(long double& d) { str t; re(t); d = stold(t); }
+tcTUU> void re(T& t, U&... u) { re(t); re(u...); }
+
+tcT> void re(complex<T>& c) { T a,b; re(a,b); c = {a,b}; }
+tcTU> void re(pair<T,U>& p) { re(p.f,p.s); }
+tcT> void re(V<T>& x) { trav(a,x) re(a); }
+tcT, size_t SZ> void re(AR<T,SZ>& x) { trav(a,x) re(a); }
+tcT> void rv(int n, V<T>& x) { x.rsz(n); re(x); }
+
+// TO_STRING
+#define ts to_string
+str ts(char c) { return str(1,c); }
+str ts(const char* s) { return (str)s; }
+str ts(str s) { return s; }
+str ts(bool b) {
+	#ifdef LOCAL
+		return b ? "true" : "false";
+	#else
+		return ts((int)b);
+	#endif
+}
+tcT> str ts(complex<T> c) {
+	stringstream ss; ss << c; return ss.str(); }
+str ts(V<bool> v) {
+	str res = "{"; ff0r(i,sz(v)) res += char('0'+v[i]);
+	res += "}"; return res; }
+template<size_t SZ> str ts(bitset<SZ> b) {
+	str res = ""; ff0r(i, SZ) res += char('0'+b[i]);
+	return res; }
+tcTU> str ts(pair<T,U> p);
+tcT> str ts(T v) { // containers with begin(), end()
+	#ifdef LOCAL
+		bool fst = 1; str res = "{";
+		for (const auto& x: v) {
+			if (!fst) res += ", ";
+			fst = 0; res += ts(x);
+		}
+		res += "}"; return res;
+	#else
+		bool fst = 1; str res = "";
+		for (const auto& x: v) {
+			if (!fst) res += " ";
+			fst = 0; res += ts(x);
+		}
+		return res;
+
+	#endif
+}
+tcTU> str ts(pair<T,U> p) {
+	#ifdef LOCAL
+		return "("+ts(p.f)+", "+ts(p.s)+")";
+	#else
+		return ts(p.f)+" "+ts(p.s);
+	#endif
+}
+
+// OUTPUT
+tcT> void pr(T x) { cout << ts(x); }
+tcTUU> void pr(const T& t, const U&... u) {
+	pr(t); pr(u...); }
+void ps() { pr("\n"); } // print w/ spaces
+tcTUU> void ps(const T& t, const U&... u) {
+	pr(t); if (sizeof...(u)) pr(" "); ps(u...); }
+
+// DEBUG
+void DBG() { cerr << "]" << endl; }
+tcTUU> void DBG(const T& t, const U&... u) {
+	cerr << ts(t); if (sizeof...(u)) cerr << ", ";
+	DBG(u...); }
+#ifdef LOCAL // compile with -DLOCAL, chk -> fake assert
+	#define dbg(...) cerr << "Line(" << __LINE__ << ") -> [" << #__VA_ARGS__ << "]: [", DBG(__VA_ARGS__)
+	#define chk(...) if (!(__VA_ARGS__)) cerr << "Line(" << __LINE__ << ") -> function(" \
+		 << __FUNCTION__  << ") -> CHK FAILED: (" << #__VA_ARGS__ << ")" << "\n", exit(0);
+#else
+	#define dbg(...) 0
+	#define chk(...) 0
+#endif
+
+// FILE I/O
+void unsyncIO() { ios_base::sync_with_stdio(false); cin.tie(nullptr); }
+
+// Note 1: He remembers no edge between A and B, but other vertices might also
+// have no edge. Therefore, the test is to see if every vertex can be connected.
+
+// Note 2: 
+
+set<int> onesib;
+int maxSize[300001];
+set<int> adj[300001];
+set<int> tot;
+
+int main() {
+	cin.tie(nullptr)->sync_with_stdio(false);
+	int n, m, k; re(n, m, k);
+
+	for (int i = 1; i <= n; i++) {
+		maxSize[i] = n - 1;
+	}
+
+	for (int i = 0; i < m; i++) {
+		int a, b; re(a, b);
+		maxSize[a]--;
+		maxSize[b]--;
+		if (a == 1) onesib.insert(b);
+		else if (b == 1) onesib.insert(a);
+		adj[a].insert(b);
+		adj[b].insert(a);
+	}
+
+	// First Check if 1 has at least k possible edge
+	if (maxSize[1] < k) {
+		cout << "impossible" << endl;
+		return 0;
+	}
+
+	// Then check if it can form a single connected component
+	// Prepare the set
+	for (int i = 2; i <= n; i++) {
+		tot.insert(i);
+	}
+
+	queue<int> other;
+
+	for (int i = 2; i <= n; i++) {
+		if (onesib.count(i)) continue;
+		else {
+			other.push(i);
+			tot.erase(i);
+		}
+	}
+
+	while (!other.empty()) {
+		int cur = other.front();
+		other.pop();
+		queue<int> to_erase;
+		for (int a : tot) {
+			if (!adj[cur].count(a)) {
+				to_erase.push(a);
+				other.push(a);
+			}
+		}
+		while (!to_erase.empty()) {
+			tot.erase(to_erase.front());
+			to_erase.pop();
+		}
+	}
+
+	if (tot.size()) {
+		cout << "impossible" << endl;
+		return 0;
+	}
+
+	// Then Check if Everyone is connected to at least one other vertex
+	bool firstTest = true;
+	for (int i = 2; i <= n; i++) {
+		if (maxSize[i] < 1) {
+			firstTest = false;
+			break;
+		}
+	}
+
+	if (!firstTest) {
+		cout << "impossible" << endl;
+		return 0;
+	}
+
+	// Now check if size can be decreased to k
+	int removable = 0;
+	auto it = onesib.begin();
+	for (int i = 2; i <= n; i++) {
+		if (it != onesib.end() && i == *it) {
+			it++;
+			continue;
+		}
+
+		if (maxSize[i] > 1) {
+			// How to check you are not going to connect it to self child
+			auto x = adj[i].begin();
+			for (int i = 2; i <= n; i++) {
+				if (x != adj[i].end() && i == *x) {
+					x++;
+					continue;
+				}
+			}
+			removable++;
+		}
+	}
+
+	if (removable >= maxSize[1] - k) {
+		cout << "possible" << endl;
+	} else {
+		cout << "impossible" << endl;
+	}
+}
