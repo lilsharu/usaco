4e536e2afd95769355b2f4741049ed5a0db7e7c2
Shourya Bansal <shouryabansal2012@gmail.com>
2021-03-28 16:49:54 -0400

    Traffic 50%

--- Preparation/HW5/Traffic/traffic.cpp
+++ Preparation/HW5/Traffic/traffic.cpp
@@ -1,5 +1,51 @@
 #include "traffic.h"
 
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+
 int LocateCentre(int N, int pp[], int S[], int D[]) {
-   return N/2;
+    vector<int> adj[N];
+
+    for (int i = 0; i < N; i++) {
+        adj[S[i]].pb(D[i]);
+        adj[D[i]].pb(S[i]);
+    }
+
+    long long cmin = LLONG_MAX;
+    int cminind = 0;
+
+    for (int i = 0; i < N; i++) {
+        vector<long long> dist(N);
+        vector<bool> vis(N);
+
+        queue<int> q;
+        q.push(i);
+
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
+            vis[cur] = true;
+
+            for (int next : adj[cur]) {
+                if (!vis[next]) {
+                    q.push(next);
+                    dist[next] = dist[cur] + 1ll;
+                }
+            }
+        }
+
+        long long tot = 0;
+        for (int i = 0; i < N; i++) {
+            tot += pp[i] * dist[i];
+        }
+
+        if (tot < cmin) {
+            cmin = tot;
+            cminind = i;
+        }
+    }
+
+    return cminind;
 }
